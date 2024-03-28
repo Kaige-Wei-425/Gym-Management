0c640f3ec618821cf494424d2ef1af32d0110961
Kaige_Wei <89125519+Kaige-Wei-425@users.noreply.github.com>
2024-03-29 09:00:56 +1100

    User Model & Schema

---
+++ src/models/user.py
@@ -0,0 +1,20 @@
+from init import db, ma
+
+# Create the user models
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+# Create user schema in different cases  
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "email", "password", "is_admin")
+
+# For handling single user
+user_shcema = UserSchema(many=True, exclude=["password"])
+# For handling multiple users
+users_shcema = UserSchema(exclude=["password"])
